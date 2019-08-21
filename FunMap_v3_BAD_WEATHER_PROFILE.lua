3b36d8a0286edc7fc95f25100f12ad94743e6c40
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2019-08-21 13:41:01 +0100

    ME weather profile for PG Bad Weather versions.

---
+++ FunMap_v3_BAD_WEATHER_PROFILE.lua
@@ -0,0 +1,71 @@
+vdata = 
+{
+    atmosphere_type = 0,
+    wind = 
+    {
+        at8000 = 
+        {
+            speed = 8.7618,
+            dir = 68,
+        }, -- end of at8000
+        at2000 = 
+        {
+            speed = 14.9466,
+            dir = 88,
+        }, -- end of at2000
+        atGround = 
+        {
+            speed = 9.7926,
+            dir = 129.99998663662,
+        }, -- end of atGround
+    }, -- end of wind
+    enable_fog = true,
+    season = 
+    {
+        temperature = 20,
+    }, -- end of season
+    enable_dust = false,
+    type_weather = 0,
+    qnh = 760.73,
+    cyclones = 
+    {
+        [1] = 
+        {
+            rotation = -0.15549016637274,
+            centerZ = -223860.07922147,
+            pressure_spread = 870220.45189726,
+            ellipticity = 0.50776093859921,
+            pressure_excess = -1322,
+            centerX = 182253.19136169,
+        }, -- end of [1]
+    }, -- end of cyclones
+    name = "PG FUN MAP BAD",
+    dust_density = 0,
+    groundTurbulence = 2.4384,
+    fog = 
+    {
+        thickness = 960.12,
+        visibility = 5619.9024,
+    }, -- end of fog
+    visibility = 
+    {
+        distance = 80000,
+    }, -- end of visibility
+    clouds = 
+    {
+        thickness = 869.8992,
+        density = 10,
+        base = 914.4,
+        iprecptns = 1,
+    }, -- end of clouds
+} -- end of vdata
+dtime = 
+{
+    date = 
+    {
+        Day = 18,
+        Year = 2011,
+        Month = 4,
+    }, -- end of date
+    start_time = 28800,
+} -- end of dtime
