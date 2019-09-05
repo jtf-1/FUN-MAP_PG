8d74ef83150db7af14c6bbc6d13514583879f293
rb05033 <35670725+rb05033@users.noreply.github.com>
2019-09-05 16:34:16 +0100

    PG Weather ME Profiles

---
+++ PG FUN MAP GOOD.lua
@@ -0,0 +1,62 @@
+vdata = 
+{
+    atmosphere_type = 0,
+    wind = 
+    {
+        at8000 = 
+        {
+            speed = 0,
+            dir = 90,
+        }, -- end of at8000
+        at2000 = 
+        {
+            speed = 0,
+            dir = 90,
+        }, -- end of at2000
+        atGround = 
+        {
+            speed = 3.0924,
+            dir = 120,
+        }, -- end of atGround
+    }, -- end of wind
+    enable_fog = false,
+    season = 
+    {
+        temperature = 20,
+    }, -- end of season
+    visibility = 
+    {
+        distance = 80000,
+    }, -- end of visibility
+    type_weather = 0,
+    qnh = 760,
+    cyclones = 
+    {
+    }, -- end of cyclones
+    name = "PG FUN MAP GOOD",
+    dust_density = 0,
+    fog = 
+    {
+        thickness = 0,
+        visibility = 25,
+    }, -- end of fog
+    groundTurbulence = 0,
+    enable_dust = false,
+    clouds = 
+    {
+        thickness = 200,
+        density = 0,
+        base = 300,
+        iprecptns = 0,
+    }, -- end of clouds
+} -- end of vdata
+dtime = 
+{
+    date = 
+    {
+        Year = 2011,
+        Day = 18,
+        Month = 4,
+    }, -- end of date
+    start_time = 28800,
+} -- end of dtime
