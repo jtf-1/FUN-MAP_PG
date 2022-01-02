5a45254399afc6913a3476c0e25966c225c31a09
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2022-01-02 19:48:29 +0000

    Missile Trainer
    - Refactored event handler.

--- scripts/Fun-Map_PG.lua
+++ scripts/Fun-Map_PG.lua
@@ -43,14 +43,18 @@ end
 --- BEGIN MISSILE TRAINER
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Create a new missile trainer object.
-MissileTrainer = {
-  menuadded = {},
-  MenuF10 = {},
-}
+-- Create event handler
+MissileTrainer = EVENTHANDLER:New()
+MissileTrainer:HandleEvent(EVENTS.Birth)
+MissileTrainer:HandleEvent(EVENTS.Dead)
 
-MissileTrainer.eventhandler = EVENTHANDLER:New()
-MissileTrainer.eventhandler:HandleEvent(EVENTS.Birth)
+
+-- Create MissileTrainer container and defaults
+MissileTrainer.menuadded = {}
+MissileTrainer.MenuF10   = {}
+MissileTrainer.safeZone = "ZONE_FOX"
+MissileTrainer.launchZone = "ZONE_FOX"
+
 
 function MissileTrainer:GetPlayerUnitAndName(unitName)
   if unitName ~= nil then
@@ -68,15 +72,6 @@ function MissileTrainer:GetPlayerUnitAndName(unitName)
   return nil,nil
 end
 
-function MissileTrainer.eventhandler:OnEventBirth(EventData)
-  local unitName = EventData.IniUnitName
-  local unit, playername = MissileTrainer:GetPlayerUnitAndName(unitName)
-  
-  if unit and playername then
-    SCHEDULER:New(nil, MissileTrainer.AddMenu, {MissileTrainer, unit, unitName},0.1)
-  end
-end
-
 MissileTrainer.fox = FOX:New() -- add new FOX class to the Missile Trainer
 
 --- FOX Default Settings
@@ -86,6 +81,8 @@ MissileTrainer.fox:SetDefaultLaunchAlerts(false) -- launcher alerts OFF
   :SetExplosionDistance(300) -- distance from uit at which to destroy incoming missiles
   :SetDebugOnOff() -- set debug on if true
   :SetDisableF10Menu() -- remove default F10 menu as a custom menu will be used
+  -- :AddSafeZone(ZONE:New(MissileTrainer.safeZone)) -- zone in which players will be protected
+  -- :AddLaunchZone(ZONE:New(MissileTrainer.launchZone)) -- zone in which launches will be tracked
   :Start() -- start the missile trainer
 
 --- Toggle Launch Alerts and Destroy Missiles on/off
@@ -96,17 +93,42 @@ function MissileTrainer:ToggleMissileTrainer(unitName)
   self.fox:_ToggleDestroyMissiles(unitName)
 end
 
---- Add Missile Trainer F10 root menu
+--- Add Missile Trainer F10 root menu.
 -- @param #MissileTrainer self
 -- @param #wrapper.Unit unit Unit object occupied by client
 -- @param #string unitName Name of unit occupied by client
-function MissileTrainer:AddMenu(unit, unitName)
+function MissileTrainer:AddMenu(unit, unitName, state)
   local group = unit:GetGroup()
   local gid = group:GetID()
 
-  self.MenuF10[gid] = missionCommands.addSubMenuForGroup(gid, "Missile Trainer")
-  rootPath = self.MenuF10[gid]
-  missionCommands.addCommandForGroup(gid, "Missile Trainer On/Off", rootPath, self.ToggleMissileTrainer, MissileTrainer, unitName)
+  if state then
+    if not self.MenuF10[gid] then
+      self.MenuF10[gid] = missionCommands.addSubMenuForGroup(gid, "Missile Trainer")
+      local rootPath = self.MenuF10[gid]
+      missionCommands.addCommandForGroup(gid, "Missile Trainer On/Off", rootPath, self.ToggleMissileTrainer, MissileTrainer, unitName)
+    end
+  else
+    self.MenuF10[gid]:Remove()
+    self.MenuF10[gid] = nil
+  end
+end
+
+function MissileTrainer:OnEventBirth(EventData)
+  local unitName = EventData.IniUnitName
+  local unit, playername = MissileTrainer:GetPlayerUnitAndName(unitName)
+  
+  if unit and playername then
+    SCHEDULER:New(nil, MissileTrainer.AddMenu, {MissileTrainer, unit, unitName, true},0.1)
+  end
+end
+
+function MissileTrainer:OnEventDead(EventData)
+  local unitName = EventData.IniUnitName
+  local unit, playername = MissileTrainer:GetPlayerUnitAndName(unitname)
+
+  if unit and playername then
+    MissileTrainer:AddMenu(unit, unitname, false)
+  end
 end
 
 --- END MISSILE TRAINER
