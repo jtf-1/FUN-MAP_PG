3b4889259261788ef95b1d40450b630a79057edf
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2024-02-08 13:07:06 +0000

    Create mission_files_TEMP.lua

---
+++ scripts/dynamic/mission_files_TEMP.lua
@@ -0,0 +1,33 @@
+-- CORE MODULES ADMIN
+__JTFLoader.Include( 'lib/Moose.lua' )                      -- library file
+__JTFLoader.Include( 'core/mission_init.lua' )              -- core file
+__JTFLoader.Include( 'mission_init_data.lua' )
+__JTFLoader.Include( 'core/devcheck.lua' )                  -- core file
+__JTFLoader.Include( 'core/missionsrs.lua' )                -- core file
+__JTFLoader.Include( 'core/adminmenu.lua' )                 -- core file
+__JTFLoader.Include( 'core/missiontimer.lua' )              -- core file
+__JTFLoader.Include( 'core/supportaircraft.lua' )           -- core file
+__JTFLoader.Include( 'core/staticranges.lua' )              -- core file
+__JTFLoader.Include( 'core/missionstrike.lua' )             -- core file
+__JTFLoader.Include( 'core/cvncontrol.lua' )                -- core file
+__JTFLoader.Include( 'core/dynamic_deck.lua' )              -- core file
+--__JTFLoader.Include( 'core/missiletrainer.lua' )          -- core file
+--__JTFLoader.Include( 'missiletrainer_data.lua' )
+__JTFLoader.Include( 'core/markspawn.lua' )                 -- core file
+__JTFLoader.Include( 'core/Hercules_cargo.lua' )            -- core file
+__JTFLoader.Include( 'core/mission_end.lua' )               -- core file
+-- TEMPLATES
+__JTFLoader.Include( 'core/spawntemplates.lua' )            -- core file
+__JTFLoader.Include( 'core/dynamic_deck_templates.lua' )    -- core file
+-- DATA
+__JTFLoader.Include( 'missionsrs_data.lua' )
+__JTFLoader.Include( 'adminmenu_data.lua' )
+__JTFLoader.Include( 'missiontimer_data.lua' )
+__JTFLoader.Include( 'supportaircraft_data.lua' )
+__JTFLoader.Include( 'staticranges_data.lua' )
+__JTFLoader.Include( 'missionstrike_data.lua' )
+__JTFLoader.Include( 'cvncontrol_data.lua' )
+__JTFLoader.Include( 'dynamic_deck_data.lua' )
+__JTFLoader.Include( 'markspawn_data.lua' )
+-- LEGACY SCRIPTS
+__JTFLoader.Include( 'legacy_mission.lua' )
