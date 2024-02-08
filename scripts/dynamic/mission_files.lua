3ad647105d06152aad763a858041c2475b6ee1a4
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2024-02-08 13:06:57 +0000

    Update mission_files.lua
    Reorder load sequence

--- scripts/dynamic/mission_files.lua
+++ scripts/dynamic/mission_files.lua
@@ -4,28 +4,31 @@ __JTFLoader.Include( 'core/mission_init.lua' )              -- core file
 __JTFLoader.Include( 'mission_init_data.lua' )
 __JTFLoader.Include( 'core/devcheck.lua' )                  -- core file
 __JTFLoader.Include( 'core/missionsrs.lua' )                -- core file
-__JTFLoader.Include( 'missionsrs_data.lua' )
 __JTFLoader.Include( 'core/adminmenu.lua' )                 -- core file
-__JTFLoader.Include( 'adminmenu_data.lua' )
 __JTFLoader.Include( 'core/missiontimer.lua' )              -- core file
-__JTFLoader.Include( 'missiontimer_data.lua' )
-__JTFLoader.Include( 'core/spawntemplates.lua' )            -- core file
 __JTFLoader.Include( 'core/supportaircraft.lua' )           -- core file
-__JTFLoader.Include( 'supportaircraft_data.lua' )
 __JTFLoader.Include( 'core/staticranges.lua' )              -- core file
-__JTFLoader.Include( 'staticranges_data.lua' )
 __JTFLoader.Include( 'core/missionstrike.lua' )             -- core file
-__JTFLoader.Include( 'missionstrike_data.lua' )
 __JTFLoader.Include( 'core/cvncontrol.lua' )                -- core file
-__JTFLoader.Include( 'cvncontrol_data.lua' )
 __JTFLoader.Include( 'core/dynamic_deck.lua' )              -- core file
-__JTFLoader.Include( 'core/dynamic_deck_templates.lua' )    -- core file
-__JTFLoader.Include( 'dynamic_deck_data.lua' )
 --__JTFLoader.Include( 'core/missiletrainer.lua' )          -- core file
 --__JTFLoader.Include( 'missiletrainer_data.lua' )
 __JTFLoader.Include( 'core/markspawn.lua' )                 -- core file
-__JTFLoader.Include( 'markspawn_data.lua' )
 __JTFLoader.Include( 'core/Hercules_cargo.lua' )            -- core file
+-- TEMPLATES
+__JTFLoader.Include( 'core/spawntemplates.lua' )            -- core file
+__JTFLoader.Include( 'core/dynamic_deck_templates.lua' )    -- core file
+-- DATA
+__JTFLoader.Include( 'missionsrs_data.lua' )
+__JTFLoader.Include( 'adminmenu_data.lua' )
+__JTFLoader.Include( 'missiontimer_data.lua' )
+__JTFLoader.Include( 'supportaircraft_data.lua' )
+__JTFLoader.Include( 'staticranges_data.lua' )
+__JTFLoader.Include( 'missionstrike_data.lua' )
+__JTFLoader.Include( 'cvncontrol_data.lua' )
+__JTFLoader.Include( 'dynamic_deck_data.lua' )
+__JTFLoader.Include( 'markspawn_data.lua' )
+-- JTF END
 __JTFLoader.Include( 'core/mission_end.lua' )               -- core file
 -- LEGACY SCRIPTS
 __JTFLoader.Include( 'legacy_mission.lua' )
